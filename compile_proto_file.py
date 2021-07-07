54c9828d7985a6474a16d42c41c7a333e68ba128
liuhui <liuhui@matrixelements.com>
2021-07-07 09:07:31 +0000

    generate .py from .proto

--- compile_proto_file.py
+++ compile_proto_file.py
@@ -1,5 +1,6 @@
-import sys
+import glob
 import os
+import sys
 
 sdk_path = sys.executable
 proto_file_path = './armada-common/Fighter'
@@ -7,10 +8,9 @@ if not os.path.exists('./armada-common/Fighter/google'):
     os.system('cp -r ./armada-common/google ./armada-common/Fighter')
 if not os.path.exists('./protos'):
     os.makedirs('./protos')
+
 base_cmd = f"{sdk_path} -m grpc_tools.protoc -I{proto_file_path} --python_out=./protos  --grpc_python_out=./protos"
-for file_name in os.listdir(proto_file_path):
-    abs_path = os.path.join(proto_file_path, file_name)
-    if os.path.isfile(abs_path):
-        protoc_cmd = f'{base_cmd} {abs_path}'
-        print(protoc_cmd)
-        os.system(protoc_cmd)
+for file_name in glob.glob(proto_file_path + '/**/*.proto', recursive=True):
+    protoc_cmd = f'{base_cmd} {file_name}'
+    print(protoc_cmd)
+    os.system(protoc_cmd)
