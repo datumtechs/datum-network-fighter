5f4d8af9c97c8591fd888f00e704ee6352c4c313
liuhui <liuhui@matrixelements.com>
2021-07-19 02:32:15 +0000

    modify proto compile script

--- compile_proto_file.py
+++ compile_proto_file.py
@@ -3,14 +3,21 @@ import os
 import sys
 
 sdk_path = sys.executable
-proto_file_path = './armada-common/Fighter'
-if not os.path.exists('./armada-common/Fighter/google'):
-    os.system('cp -r ./armada-common/google ./armada-common/Fighter')
+proto_root_path = './armada-common'
+proto_include_path1 = os.path.join(proto_root_path, 'Fighter')
+proto_include_path2 = os.path.join(proto_root_path, 'Carrier')
+
+protos_to_compile1 = os.path.join(proto_include_path1, '*.proto')
+protos_to_compile2 = os.path.join(proto_include_path2, 'lib/api/*.proto')
+
 if not os.path.exists('./protos'):
     os.makedirs('./protos')
 
-base_cmd = f"{sdk_path} -m grpc_tools.protoc -I{proto_file_path} --python_out=./protos  --grpc_python_out=./protos"
-for file_name in glob.glob(proto_file_path + '/**/*.proto', recursive=True):
-    protoc_cmd = f'{base_cmd} {file_name}'
-    print(protoc_cmd)
-    os.system(protoc_cmd)
+base_cmd = f"{sdk_path} -m grpc_tools.protoc --python_out=./protos --grpc_python_out=./protos"
+
+cmd1 = f'{base_cmd} -I{proto_include_path1} -I{proto_root_path} {protos_to_compile1}'
+print(cmd1)
+os.system(cmd1)
+cmd2 = f'{base_cmd} -I{proto_include_path2} -I{proto_root_path} {protos_to_compile2}'
+print(cmd2)
+os.system(cmd2)
