9952f1abb47669114c53c1ebd4a04f8b9d93e6bd
liyuanchao <liyuanchao@matrixelements.com>
2021-12-30 14:06:00 +0800

    add check grpc state interface

--- common/utils.py
+++ common/utils.py
@@ -1,4 +1,5 @@
 import yaml
+from grpc import _common
 
 
 def load_cfg(file):
@@ -12,3 +13,8 @@ def load_cfg(file):
 
 def dump_yaml(data, file_handle):
     yaml.dump(data, file_handle, default_flow_style=False)
+
+
+def check_grpc_channel_state(channel, try_to_connect=True):
+    result = channel._channel.check_connectivity_state(try_to_connect)
+    return _common.CYGRPC_CONNECTIVITY_STATE_TO_CHANNEL_CONNECTIVITY[result]
